ad945e7bad6d4664a0461f978ba9770270bcf428
markheger <mark-oliver.heger@de.ibm.com>
2019-09-04 07:34:09 +0000

    1.3.3

--- package/streamsx/hbase/_hbase.py
+++ package/streamsx/hbase/_hbase.py
@@ -69,10 +69,10 @@ def _generate_hbase_site_xml(topo, connection=None):
         host = HostPort[0]
         port = HostPort[1]
         script_dir = os.path.dirname(os.path.realpath(__file__))
-        hbaseSiteTemplate=script_dir + '/hbase-site.xml.temp'
+        hbaseSiteTemplate=script_dir + '/hbase-site.xml.template'
         hbaseSiteXmlFile=gettempdir()+'/hbase-site.xml'
 
-        # reads the hbase-site.xml.temp and replase the host and port
+        # reads the hbase-site.xml.template and replase the host and port
         with open(hbaseSiteTemplate) as f:
             newText=f.read().replace('HOST_NAME', host)
             newText=newText.replace('PORT', port)
